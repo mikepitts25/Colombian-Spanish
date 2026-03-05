02f4b16b5bece1755bde4f4d284400177ee5c113
Ubuntu <ubuntu@ip-172-31-42-3.eu-central-1.compute.internal>
2026-03-05 21:51:46 +0000

    Wrap app with GestureHandlerRootView to fix gesture detector error

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import RootNavigator from './src/navigation';
 import { DeckProvider } from './src/hooks/useDeck';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <DeckProvider>
-        <StatusBar style="light" />
-        <RootNavigator />
-      </DeckProvider>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <DeckProvider>
+          <StatusBar style="light" />
+          <RootNavigator />
+        </DeckProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
